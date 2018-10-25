02349ef99367b06b7b032305c8e0477267a9e321
Josh Zimmerman <JoshuaZimmerman@quickenloans.com>
2018-10-25 13:41:45 -0400

    this is my first commit for a set of utilities I've written for use with console applications, from console menu display and entry, as well as input validation.

---
+++ MenuHandling.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBuild.Utilities {
+    class MenuHandling {
+
+        public static void PrintMenuOptions(IList<object> menuOptions, [Optional]bool menuMode) {
+
+            for (int i = 0; i < menuOptions.Count; i++) {
+                Console.WriteLine((menuMode? $"{i + 1}.) " : "") + menuOptions[i]);
+            }
+        }
+
+        public static uint SelectMenuOption(int numberOfOptions) {
+            uint userSelection = 0;
+            string userEntry = "";
+
+            while ( !uint.TryParse(userEntry, out userSelection) || 
+                    userSelection < 0 || 
+                    userSelection > numberOfOptions) {
+                UserInput.PromptUntilValidEntry($"Please enter a selection, 1-{numberOfOptions}: ", out userEntry, InformationType.Numeric);
+            }
+            return userSelection;
+        }
+    }
+}
